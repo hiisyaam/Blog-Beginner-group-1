b080faa5e59bb6d3d9eebcbba7a7825418ae4cb5
Abigail <farishiisyamhardimann@gmail.com>
2024-12-08 12:38:42 +0700

    Menambahkan file baru: assets, vendor, index.blade.php, layouts.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts')
+
+@section('judul', 'Selamat Datang')
+
+@section('content')
+    <h1>Selamat Datang di AAHI</h1>
+    <p>Isi Disini Brokk</p>
+@endsection
---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>@yield('judul','Ada Apa Hari Ini')</title>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+  </head>
+
+<body>
+
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <a href="/" class="logo">
+                        <h2 style="color: #ec6090;">AAHI</h2>
+                        <p>Ada Apa Hari Ini?</p>
+                    </a>
+                    <ul class="nav">
+                        <li><a href="#" class="active">Kita Aslinya 3</a></li>
+                        <li><a href="#">Artikel</a></li>
+                        @if (Auth::check())
+                          <li><a href="">Profile</a></li>
+                        @endif
+                        <li><a href="/login">Login <img src="assets/images/log-in.png" alt=""></a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+          @yield('content');
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Kelompok 1 Aja Deh</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+  </body>
+
+</html>
